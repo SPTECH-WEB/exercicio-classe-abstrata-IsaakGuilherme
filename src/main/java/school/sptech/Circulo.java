b36b69a9c6b9f583f257bd67c0ca4f3152ff40e5
IsaakGuilherme <isaak.guilherme@sptech.school>
2025-10-31 12:49:50 -0300

    Exercício finalizado - Isaak Guilherme - RA: 04251112

--- src/main/java/school/sptech/Circulo.java
+++ src/main/java/school/sptech/Circulo.java
@@ -1,4 +1,27 @@
 package school.sptech;
 
-public class Circulo {
+public class Circulo extends Figura{
+    private Double raio;
+
+    public Circulo(){
+
+    }
+
+    public Double getRaio() {
+        return raio;
+    }
+
+    public void setRaio(Double raio) {
+        this.raio = raio;
+    }
+
+    @Override
+    public Double calcularArea(){
+        return Math.pow(getRaio(), 2) * Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "Área do circulo é: " + calcularArea();
+    }
 }
--- src/main/java/school/sptech/Imagem.java
+++ src/main/java/school/sptech/Imagem.java
@@ -1,4 +1,49 @@
 package school.sptech;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Imagem {
+    private List<Figura> figuras;
+
+    public Imagem() {
+        this.figuras = new ArrayList<>();
+    }
+
+    public void adicionar(Figura figura){
+        figuras.add(figura);
+    }
+
+    public Double calcularSomaDasAreas(){
+        Double somaFiguras = 0.0;
+        for (Figura d: figuras){
+            somaFiguras += d.calcularArea();
+        }
+
+        return somaFiguras;
+    }
+
+    public List<Figura> buscarPorAreaMaiorQue20(){
+        List<Figura> figurasMaioque20 = new ArrayList<>();
+
+        for (int i = 0; i < figuras.size(); i++){
+            if (figuras.get(i).calcularArea() > 20.0){
+                figurasMaioque20.add(figuras.get(i));
+            }
+        }
+
+        return figurasMaioque20;
+    }
+
+    public List<Figura> buscarQuadrados(){
+        List<Figura> quadradosFigura = new ArrayList<>();
+
+        for (int i = 0; i < figuras.size(); i++){
+            if (figuras.get(i) instanceof Quadrado){
+                quadradosFigura.add(figuras.get(i));
+            }
+        }
+
+        return quadradosFigura;
+    }
 }
